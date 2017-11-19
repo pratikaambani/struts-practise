3292878c7c31cd0007334ebc0ec48a30094c31a3
Pratik Ambani <pratikaambani@gmail.com>
2017-11-19 14:03:49 +0530

    Struts Practise Programs 9

---
+++ Struts2Test5Interceptor/src/com/Login.java
@@ -0,0 +1,14 @@
+package com;  
+public class Login {  
+private String name;  
+  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String execute(){  
+    return "success";  
+}  
+}  
